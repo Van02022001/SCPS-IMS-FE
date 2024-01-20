c7a7921dfa48a3b24077765e2437dfa00ba2c09c
Hoang Phi Khanh <phikhanh0169@gmail.com>
2024-01-20 14:33:31 +0700

    [Fix] api

--- admin-casptone-project/src/data/api/axios.jsx
+++ admin-casptone-project/src/data/api/axios.jsx
@@ -4,15 +4,16 @@ import axios from "axios";
 const token = localStorage.getItem('token')
 
 const BaseURL = "http://localhost:8080/api/v1"
+const BaseURLWeb = "https://phu-tung-bom-be-tong-sg.azurewebsites.net/api/v1"
 
 export const axiosInstance = axios.create({
-  baseURL: BaseURL,
+  baseURL: BaseURLWeb,
   timeout: 30000,
   headers: {
     common: {
       "Content-Type": "application/json",
+      "Authorization": `Bearer ${localStorage.getItem('token')}`,
     },
-    authorization: `Bearer ${token}`,
   },
 });
 
@@ -22,7 +23,23 @@ export const imageInstance = axios.create({
   headers: {
     common: {
       "Content-Type": "multipart/form-data",
+      "Authorization": `Bearer ${localStorage.getItem('token')}`,
     },
-    authorization: `Bearer ${token}`,
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+    return config;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Xử lý lỗi 401, ví dụ chuyển hướng đến trang đăng nhập
+      window.location.href = '/login';
+    } else {
+      console.error('Response interceptor error:', error);
+    }
+    return Promise.reject(error);
+  }
+);
